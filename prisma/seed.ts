a6975aa3e8d53197a38ff3be1358dc5724bb8ab8
Daniel Sprohar <daniel.sprohar@outlook.com>
2023-02-09 13:23:21 -0600

    Seed db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,2 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
+
+const main = async () => {
+  const projects = await prisma.project.create({
+    data: {
+      name: 'Yata API',
+    },
+  });
+
+  await prisma.task.createMany({
+    data: [
+      { name: 'Create controllers', projectId: projects.id },
+      { name: 'Create services', projectId: projects.id },
+      { name: 'e2e tests', projectId: projects.id },
+    ],
+  });
+};
+
+main()
+  .then(() => {
+    console.log('Seeded the database');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
