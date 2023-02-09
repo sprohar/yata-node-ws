30c5ba4c5df3f50eb6b9ee3fba8cae6ec2c08203
Daniel Sprohar <daniel.sprohar@outlook.com>
2023-02-09 08:36:22 -0600

    Prisma recipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [ProjectsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
