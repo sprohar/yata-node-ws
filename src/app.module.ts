4b1555613ce5f3b88a2df4b3eb9cf3162bf7c43b
Daniel Sprohar <daniel.sprohar@outlook.com>
2023-02-09 15:18:50 -0600

    Removed auto-generated code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [ProjectsModule, PrismaModule, TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
