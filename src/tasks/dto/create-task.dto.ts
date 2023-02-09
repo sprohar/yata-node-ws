f2c5b1ddd0dfcf7fc683d35a3803577eab31cb45
Daniel Sprohar <daniel.sprohar@outlook.com>
2023-02-09 13:24:09 -0600

    Validation for Task dto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,32 @@
-export class CreateTaskDto {}
+import { Priority } from '@prisma/client';
+import {
+  IsEnum,
+  IsISO8601,
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  MaxLength,
+} from 'class-validator';
+import { Task } from '../entities/task.entity';
+
+export class CreateTaskDto {
+  @IsNotEmpty()
+  @MaxLength(Task.Name.MAX_LENGTH)
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  projectId: number;
+
+  @IsOptional()
+  @MaxLength(Task.Description.MAX_LENGTH)
+  description?: string;
+
+  @IsISO8601()
+  @IsOptional()
+  dueDate?: string;
+
+  @IsOptional()
+  @IsEnum(Priority)
+  priority?: Priority;
+}
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,18 @@
 export class Task {}
+
+export namespace Task {
+  export enum Name {
+    MAX_LENGTH = 256,
+  }
+
+  export enum Description {
+    MAX_LENGTH = 8192,
+  }
+
+  export enum OrderBy {
+    NAME = 'name',
+    PRIORITY = 'priority',
+    DUE_DATE = 'dueDate',
+    INCEPTION = 'createdAt'
+  }
+}
