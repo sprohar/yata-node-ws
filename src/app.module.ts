f78f86d1888ba7fd598e586a51a068d01bdd0e94
Daniel Sprohar <daniel.sprohar@outlook.com>
2023-02-22 21:15:32 -0600

    Added a throttler guard for rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { ProjectsModule } from './projects/projects.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler/dist/throttler.guard';
 
 @Module({
-  imports: [ProjectsModule, PrismaModule, TasksModule],
+  imports: [
+    ProjectsModule,
+    PrismaModule,
+    TasksModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
