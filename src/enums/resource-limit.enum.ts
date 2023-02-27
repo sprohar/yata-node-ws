43a28d8141433f26b67c9ee86b65a83945535a1c
Daniel Sprohar <daniel.sprohar@outlook.com>
2023-02-27 09:14:40 -0600

    Enum for resource limits

---
+++ src/enums/resource-limit.enum.ts
@@ -0,0 +1,14 @@
+export enum ResourceLimit {
+  /**
+   * The maximum number of projects that a user can have.
+   */
+  PROJECT = 10,
+  /**
+   * The maximum number of sections that a project can have.
+   */
+  SECTION = 30,
+  /**
+   * The maximum number of tasks that a project can have.
+   */
+  TASK = 100,
+}
---
+++ src/exceptions/resource-limit.exception.ts
@@ -0,0 +1,5 @@
+export class ResourceLimitException extends Error {
+  constructor(message?: string) {
+    super(message);
+  }
+}
