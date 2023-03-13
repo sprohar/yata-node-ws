13fd72eaf3f1d736c940ad8fda18edc78ce00193
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-13 11:40:21 -0500

    Removed the chrono filter endpoint from the Tasks controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,17 +10,12 @@ import {
   ParseIntPipe,
   Patch,
   Post,
-  Query,
 } from '@nestjs/common';
 import { BadRequestException } from '@nestjs/common/exceptions';
 import { ApiTags } from '@nestjs/swagger/dist/decorators';
-import { Prisma } from '@prisma/client';
-import { QueryParams } from '../dto/query-params.dto';
 import { ProjectsService } from '../projects/projects.service';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { TasksChronoQueryParamsDto } from './dto/tasks-chrono-query-params.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import { Task } from './entities/task.entity';
 import { TasksService } from './tasks.service';
 
 @ApiTags('Tasks')
@@ -42,86 +37,6 @@ export class TasksController {
     }
 
     return await this.tasksService.create(createTaskDto);
-  }
-
-  @Get('chrono')
-  async filterByDate(
-    @Param('projectId', ParseIntPipe) projectId: number,
-    @Query() query: TasksChronoQueryParamsDto,
-  ) {
-    const skip = query.skip ?? QueryParams.SKIP_DEFAULT;
-    const take = query.take ?? QueryParams.TAKE_DEFAULT;
-    const orderBy: { [key: string]: string } = {};
-    orderBy[`${query.orderBy ?? Task.OrderBy.DEFAULT}`] =
-      query.dir ?? Prisma.SortOrder.asc;
-
-    let where: Prisma.TaskWhereInput = {
-      projectId,
-    };
-
-    if (query.start && query.end) {
-      where = {
-        ...where,
-        dueDate: {
-          gte: query.start,
-          lte: query.end,
-        },
-      };
-    } else if (query.start && !query.end) {
-      where = {
-        ...where,
-        dueDate: {
-          gte: query.start,
-        },
-      };
-    } else if (!query.start && query.end) {
-      where = {
-        ...where,
-        dueDate: {
-          lte: query.end,
-        },
-      };
-    }
-
-    return await this.tasksService.findAll({
-      skip,
-      take,
-      where,
-      orderBy,
-    });
-  }
-
-  @Get('chrono/today')
-  async getTodaysTasks(
-    @Param('projectId', ParseIntPipe) projectId: number,
-    @Query() query: QueryParams,
-  ) {
-    const skip = query.skip ?? QueryParams.SKIP_DEFAULT;
-    const take = query.take ?? QueryParams.TAKE_DEFAULT;
-    const upperBound = new Date();
-    upperBound.setHours(23);
-    upperBound.setMinutes(59);
-    upperBound.setSeconds(0);
-
-    const lowerBound = new Date();
-    lowerBound.setHours(0);
-    lowerBound.setMinutes(0);
-    lowerBound.setSeconds(0);
-
-    return await this.tasksService.findAll({
-      skip,
-      take,
-      orderBy: {
-        dueDate: Prisma.SortOrder.asc,
-      },
-      where: {
-        projectId,
-        dueDate: {
-          lte: upperBound,
-          gte: lowerBound,
-        },
-      },
-    });
   }
 
   @Get(':id')
