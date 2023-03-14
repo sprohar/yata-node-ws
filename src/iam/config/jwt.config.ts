9e25f4d70653ccf39c92b410fbec7a0a0fa08735
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-14 15:14:15 -0500

    Modified the ttl vars to be of type number

--- src/iam/config/jwt.config.ts
+++ src/iam/config/jwt.config.ts
@@ -7,6 +7,6 @@ export default registerAs('jwt', () => ({
   secret: process.env.JWT_SECRET,
   audience: process.env.JWT_TOKEN_AUDIENCE,
   issuer: process.env.JWT_TOKEN_ISSUER,
-  accessTokenTtl: process.env.JWT_ACCESS_TOKEN_TTL,
-  refreshTokenTtl: process.env.JWT_REFRESH_TOKEN_TTL,
+  accessTokenTtl: parseInt(process.env.JWT_ACCESS_TOKEN_TTL ?? '1200'),
+  refreshTokenTtl: parseInt(process.env.JWT_REFRESH_TOKEN_TTL ?? '86400'),
 }));
