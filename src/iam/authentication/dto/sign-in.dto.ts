5f03fc5aa0b453f066cb3c079c35f6729439fc45
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-14 15:14:49 -0500

    Use enum constant for max_length value in dto validation

--- src/iam/authentication/dto/sign-in.dto.ts
+++ src/iam/authentication/dto/sign-in.dto.ts
@@ -1,9 +1,10 @@
 import { IsEmail, MinLength } from "class-validator";
+import { User } from "../../../users/entities/user.entity";
 
 export class SignInDto {
   @IsEmail()
   email: string;
 
-  @MinLength(10)
+  @MinLength(User.Password.MinLength)
   password: string;
 }
