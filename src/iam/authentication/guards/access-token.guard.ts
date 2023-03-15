8817d723e03e94d9f9e3581a99c37b4c368a5d3e
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-15 18:36:14 -0500

    Relative imports ... only a sith deals in absolutes

--- src/iam/authentication/guards/access-token.guard.ts
+++ src/iam/authentication/guards/access-token.guard.ts
@@ -8,8 +8,8 @@ import {
 import { ConfigType } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import jwtConfig from 'src/iam/config/jwt.config';
-import { REQUEST_USER_KEY } from 'src/iam/iam.constants';
+import jwtConfig from '../../config/jwt.config';
+import { REQUEST_USER_KEY } from '../../iam.constants';
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
@@ -19,17 +19,18 @@ export class AccessTokenGuard implements CanActivate {
     private jwtService: JwtService,
   ) {}
 
-  async canActivate(
-    context: ExecutionContext,
-  ):Promise<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException();
     }
-    
+
     try {
-      const payload = await this.jwtService.verifyAsync(token, this.jwtConfiguration);
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        this.jwtConfiguration,
+      );
       request[REQUEST_USER_KEY] = payload;
     } catch (error) {
       console.error(error);
