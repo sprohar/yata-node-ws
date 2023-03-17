aa425203592a1785704815227ffe0215663c4ccc
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-16 19:01:14 -0500

    Get the user_id from the req object when fetching tasks for the e-matrix

--- src/tasks/eisenhower-matrix.controller.ts
+++ src/tasks/eisenhower-matrix.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Prisma } from '@prisma/client';
 import { QueryParams } from '../dto/query-params.dto';
 import { TasksService } from './tasks.service';
+import { ActiveUser } from '../iam/decorators/active-user.decorator';
 
 @ApiTags('Eisenhower')
 @Controller('tasks')
@@ -10,7 +11,9 @@ export class EisenhowerMatrixController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  async getAll(@Query() query: QueryParams) {
+  async getAll(
+    @ActiveUser('sub', ParseIntPipe) userId: number,
+    @Query() query: QueryParams) {
     const { skip, take } = query;
 
     return await this.tasksService.findAll({
@@ -19,6 +22,9 @@ export class EisenhowerMatrixController {
       orderBy: {
         createdAt: Prisma.SortOrder.asc,
       },
+      where: {
+        userId,
+      }
     });
   }
 }
