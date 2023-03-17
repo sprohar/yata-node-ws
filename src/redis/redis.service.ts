aff7b04b61444d54af0ecc08e972bfb10a184128
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-16 19:00:41 -0500

    Fixed an error when starting initializing the RedisService

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -7,7 +7,7 @@ export class RedisService extends Redis {
   constructor(config: ConfigService) {
     super({
       host: config.get('REDIS_HOST'),
-      port: Number.parseInt(config.get('REDIS_PORT'), 10) ?? 6379,
+      port: parseInt(config.get('REDIS_PORT') ?? '6379'),
     });
   }
 }
