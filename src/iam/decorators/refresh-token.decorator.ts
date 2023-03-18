c44cde67edce310a05aa567b27adf78c2baf66a5
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-18 10:33:13 -0500

    Created a custom decorator to pick cookies from the cookie jar

---
+++ src/iam/decorators/refresh-token.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { COOKIE_REFRESH_TOKEN_KEY } from '../iam.constants';
+
+export const RefreshToken = createParamDecorator(
+  (_data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.cookies[COOKIE_REFRESH_TOKEN_KEY];
+  },
+);
