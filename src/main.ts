0cafd2fac7b345abc79a5f7a320ab396e636a7ab
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-18 18:39:24 -0500

    Changed the fallback value for the app port

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(parseInt(process.env.APP_PORT ?? '7070'));
+  await app.listen(parseInt(process.env.APP_PORT ?? '3000'));
 }
 bootstrap();
