a4131870903214bf5abfefd04d56efb78403c496
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-24 23:58:39 -0500

    More seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -38,7 +38,7 @@ async function main() {
     },
   });
 
-  const projects = await prisma.project.create({
+  const projectYataApi = await prisma.project.create({
     data: {
       name: 'Yata API',
       userId: user.id,
@@ -47,38 +47,52 @@ async function main() {
           { name: 'To-Do' },
           { name: 'In Progress' },
           { name: 'Testing' },
-          { name: 'Complete' },
         ] as Section[],
       },
     },
   });
 
+  const projectYataSpa = await prisma.project.create({
+    data: {
+      name: 'Yata SPA',
+      userId: user.id,
+      sections: {
+        create: [
+          { name: 'To-Do' },
+          { name: 'In Progress' },
+          { name: 'Testing' },
+        ] as Section[],
+      },
+    },
+  });
+
+  // Create 20 tasks for project 1
   await prisma.task.createMany({
     data: [
       {
         title: 'Create controllers',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 4,
         userId: user.id,
         dueDate: today,
       },
       {
         title: 'Create services',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 4,
         userId: user.id,
         dueDate: today,
       },
       {
         title: 'e2e tests',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 4,
         userId: user.id,
         dueDate: today,
       },
       {
         title: 'High priority task 1',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 2,
         userId: user.id,
         priority: Task.Priority.HIGH,
@@ -86,7 +100,7 @@ async function main() {
       },
       {
         title: 'High priority task 2',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 2,
         userId: user.id,
         priority: Task.Priority.HIGH,
@@ -94,7 +108,7 @@ async function main() {
       },
       {
         title: 'High priority task 3',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 2,
         userId: user.id,
         priority: Task.Priority.HIGH,
@@ -102,7 +116,7 @@ async function main() {
       },
       {
         title: 'High priority task 4',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 2,
         userId: user.id,
         priority: Task.Priority.HIGH,
@@ -110,7 +124,7 @@ async function main() {
       },
       {
         title: 'High priority task 5',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 2,
         userId: user.id,
         priority: Task.Priority.HIGH,
@@ -118,7 +132,7 @@ async function main() {
       },
       {
         title: 'High priority task 6',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 2,
         userId: user.id,
         priority: Task.Priority.HIGH,
@@ -126,7 +140,7 @@ async function main() {
       },
       {
         title: 'Medium priority task 1',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 1,
         userId: user.id,
         priority: Task.Priority.MEDIUM,
@@ -135,7 +149,7 @@ async function main() {
       },
       {
         title: 'Medium priority task 2',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 1,
         userId: user.id,
         priority: Task.Priority.MEDIUM,
@@ -144,7 +158,7 @@ async function main() {
       },
       {
         title: 'Medium priority task 3',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 1,
         userId: user.id,
         priority: Task.Priority.MEDIUM,
@@ -153,7 +167,7 @@ async function main() {
       },
       {
         title: 'Medium priority task 4',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 1,
         userId: user.id,
         priority: Task.Priority.MEDIUM,
@@ -162,7 +176,7 @@ async function main() {
       },
       {
         title: 'Medium priority task 5',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 1,
         userId: user.id,
         priority: Task.Priority.MEDIUM,
@@ -171,7 +185,7 @@ async function main() {
       },
       {
         title: 'Medium priority task 6',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 1,
         userId: user.id,
         priority: Task.Priority.MEDIUM,
@@ -180,28 +194,199 @@ async function main() {
       },
       {
         title: 'Low priority task 1',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 3,
         userId: user.id,
         priority: Task.Priority.LOW,
       },
       {
         title: 'Low priority task 2',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 3,
         userId: user.id,
         priority: Task.Priority.LOW,
       },
       {
         title: 'Low priority task 3',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
         sectionId: 3,
         userId: user.id,
         priority: Task.Priority.LOW,
       },
       {
         title: 'Low priority task 4',
-        projectId: projects.id,
+        projectId: projectYataApi.id,
+        sectionId: 3,
+        userId: user.id,
+        priority: Task.Priority.LOW,
+      },
+      {
+        title: 'Low priority task 5',
+        projectId: projectYataApi.id,
+        sectionId: 3,
+        userId: user.id,
+        priority: Task.Priority.LOW,
+      },
+    ],
+  });
+
+  // Create 20 tasks for project 2
+  await prisma.task.createMany({
+    data: [
+      {
+        title: 'Create controllers',
+        projectId: projectYataSpa.id,
+        sectionId: 4,
+        userId: user.id,
+        dueDate: today,
+      },
+      {
+        title: 'Create services',
+        projectId: projectYataSpa.id,
+        sectionId: 4,
+        userId: user.id,
+        dueDate: today,
+      },
+      {
+        title: 'e2e tests',
+        projectId: projectYataSpa.id,
+        sectionId: 4,
+        userId: user.id,
+        dueDate: today,
+      },
+      {
+        title: 'High priority task 1',
+        projectId: projectYataSpa.id,
+        sectionId: 2,
+        userId: user.id,
+        priority: Task.Priority.HIGH,
+        dueDate: today,
+      },
+      {
+        title: 'High priority task 2',
+        projectId: projectYataSpa.id,
+        sectionId: 2,
+        userId: user.id,
+        priority: Task.Priority.HIGH,
+        dueDate: today,
+      },
+      {
+        title: 'High priority task 3',
+        projectId: projectYataSpa.id,
+        sectionId: 2,
+        userId: user.id,
+        priority: Task.Priority.HIGH,
+        dueDate: today,
+      },
+      {
+        title: 'High priority task 4',
+        projectId: projectYataSpa.id,
+        sectionId: 2,
+        userId: user.id,
+        priority: Task.Priority.HIGH,
+        dueDate: tomorrow,
+      },
+      {
+        title: 'High priority task 5',
+        projectId: projectYataSpa.id,
+        sectionId: 2,
+        userId: user.id,
+        priority: Task.Priority.HIGH,
+        dueDate: tomorrow,
+      },
+      {
+        title: 'High priority task 6',
+        projectId: projectYataSpa.id,
+        sectionId: 2,
+        userId: user.id,
+        priority: Task.Priority.HIGH,
+        dueDate: tomorrow,
+      },
+      {
+        title: 'Medium priority task 1',
+        projectId: projectYataSpa.id,
+        sectionId: 1,
+        userId: user.id,
+        priority: Task.Priority.MEDIUM,
+        dueDate: dayAfterTomorrow,
+        content: 'This can wait until next week',
+      },
+      {
+        title: 'Medium priority task 2',
+        projectId: projectYataSpa.id,
+        sectionId: 1,
+        userId: user.id,
+        priority: Task.Priority.MEDIUM,
+        dueDate: dayAfterTomorrow,
+        content: 'This can wait until next week',
+      },
+      {
+        title: 'Medium priority task 3',
+        projectId: projectYataSpa.id,
+        sectionId: 1,
+        userId: user.id,
+        priority: Task.Priority.MEDIUM,
+        dueDate: dayAfterTomorrow,
+        content: 'This can wait until next week',
+      },
+      {
+        title: 'Medium priority task 4',
+        projectId: projectYataSpa.id,
+        sectionId: 1,
+        userId: user.id,
+        priority: Task.Priority.MEDIUM,
+        dueDate: nextWeek,
+        content: 'This can wait until next week',
+      },
+      {
+        title: 'Medium priority task 5',
+        projectId: projectYataSpa.id,
+        sectionId: 1,
+        userId: user.id,
+        priority: Task.Priority.MEDIUM,
+        dueDate: nextWeek,
+        content: 'This can wait until next week',
+      },
+      {
+        title: 'Medium priority task 6',
+        projectId: projectYataSpa.id,
+        sectionId: 1,
+        userId: user.id,
+        priority: Task.Priority.MEDIUM,
+        dueDate: nextWeek,
+        content: 'This can wait until next week',
+      },
+      {
+        title: 'Low priority task 1',
+        projectId: projectYataSpa.id,
+        sectionId: 3,
+        userId: user.id,
+        priority: Task.Priority.LOW,
+      },
+      {
+        title: 'Low priority task 2',
+        projectId: projectYataSpa.id,
+        sectionId: 3,
+        userId: user.id,
+        priority: Task.Priority.LOW,
+      },
+      {
+        title: 'Low priority task 3',
+        projectId: projectYataSpa.id,
+        sectionId: 3,
+        userId: user.id,
+        priority: Task.Priority.LOW,
+      },
+      {
+        title: 'Low priority task 4',
+        projectId: projectYataSpa.id,
+        sectionId: 3,
+        userId: user.id,
+        priority: Task.Priority.LOW,
+      },
+      {
+        title: 'Low priority task 5',
+        projectId: projectYataSpa.id,
         sectionId: 3,
         userId: user.id,
         priority: Task.Priority.LOW,
@@ -231,7 +416,14 @@ async function main() {
       name: 'C++',
       userId: user.id,
       tasks: {
-        connect: [{ id: 1 }, { id: 2 }, { id: 3 }],
+        connect: [
+          { id: 1 },
+          { id: 2 },
+          { id: 3 },
+          { id: 21 },
+          { id: 22 },
+          { id: 23 },
+        ],
       },
     },
   });
@@ -241,7 +433,14 @@ async function main() {
       name: 'Java',
       userId: user.id,
       tasks: {
-        connect: [{ id: 4 }, { id: 5 }, { id: 6 }],
+        connect: [
+          { id: 4 },
+          { id: 5 },
+          { id: 6 },
+          { id: 24 },
+          { id: 25 },
+          { id: 26 },
+        ],
       },
     },
   });
@@ -251,7 +450,14 @@ async function main() {
       name: 'TypeScript',
       userId: user.id,
       tasks: {
-        connect: [{ id: 7 }, { id: 8 }, { id: 9 }],
+        connect: [
+          { id: 7 },
+          { id: 8 },
+          { id: 9 },
+          { id: 27 },
+          { id: 28 },
+          { id: 29 },
+        ],
       },
     },
   });
@@ -261,7 +467,14 @@ async function main() {
       name: 'SQL',
       userId: user.id,
       tasks: {
-        connect: [{ id: 10 }, { id: 11 }, { id: 12 }],
+        connect: [
+          { id: 10 },
+          { id: 11 },
+          { id: 12 },
+          { id: 30 },
+          { id: 31 },
+          { id: 32 },
+        ],
       },
     },
   });
