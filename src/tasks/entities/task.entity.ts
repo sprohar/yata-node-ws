0ead6f89223294351687612f04053282e12cbdd5
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-25 19:30:09 -0500

    Made the enum values explicit

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -2,12 +2,12 @@ export class Task {}
 
 export namespace Task {
   export enum Priority {
-    NONE,
-    HIGH,
-    MEDIUM,
-    LOW,
+    NONE = 0,
+    HIGH = 1,
+    MEDIUM = 2,
+    LOW = 3,
   }
-  
+
   export enum Title {
     MAX_LENGTH = 1024,
   }
@@ -21,6 +21,6 @@ export namespace Task {
     PRIORITY = 'priority',
     DUE_DATE = 'dueDate',
     INCEPTION = 'createdAt',
-    DEFAULT = 'createdAt'
+    DEFAULT = 'createdAt',
   }
 }
