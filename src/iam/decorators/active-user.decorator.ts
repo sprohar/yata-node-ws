c9415980789afe548894f8061fa2ad6d487b0050
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-20 19:13:25 -0500

    Modified the active-user decorator to user oauth dep

--- src/iam/decorators/active-user.decorator.ts
+++ src/iam/decorators/active-user.decorator.ts
@@ -1,6 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { ActiveUserData } from '../active-user-data';
-import { REQUEST_USER_KEY } from '../iam.constants';
 
 export const ActiveUser = createParamDecorator(
   /**
@@ -11,8 +10,11 @@ export const ActiveUser = createParamDecorator(
    * otherwise the `user` property from the `Request` object is returned.
    */
   (field: keyof ActiveUserData, ctx: ExecutionContext) => {
+    // https://www.npmjs.com/package/express-oauth2-jwt-bearer
     const request = ctx.switchToHttp().getRequest();
-    const user: ActiveUserData | undefined = request[REQUEST_USER_KEY];
-    return field ? user?.[field] : user;
+    // const user: ActiveUserData | undefined = request[REQUEST_USER_KEY];
+    const auth = request['auth'];
+    const payload = auth.payload;
+    return field ? payload?.[field] : payload;
   },
 );
