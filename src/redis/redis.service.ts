67523c6388a2f4e34cf803afa8d2f3cf2a280529
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-21 21:58:53 -0500

    Disconnect from redis on application shutdown

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationShutdown } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Redis } from 'ioredis';
 
 @Injectable()
-export class RedisService extends Redis {
+export class RedisService extends Redis implements OnApplicationShutdown {
   constructor(config: ConfigService) {
     super({
       host: config.get('REDIS_HOST') ?? 'localhost',
       port: parseInt(config.get('REDIS_PORT') ?? '6379'),
     });
+  }
+
+  onApplicationShutdown(_signal?: string) {
+    this.quit();
   }
 }
